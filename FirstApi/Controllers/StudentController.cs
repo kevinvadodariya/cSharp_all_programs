aec6ea67be5c57a199468dc5bc70609e7fb54874
Kevin Patel <kevinvadodariya3268@gmail.com>
2026-01-06 19:42:19 +0530

    Add Swagger and Create First Get Api by id  and Rollno

---
+++ FirstApi/Controllers/StudentController.cs
@@ -0,0 +1,56 @@
+﻿using FirstApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        public static List<Student> students = new List<Student> {
+
+        new Student{id=1,StudentName="Kevin",FatherName="Viththalbhai",Rollno=12},
+        new Student{ id=2,StudentName="Meet",FatherName="Sanjaybhai",Rollno=8} 
+        };
+
+        [HttpGet]//get all recoards
+        public async Task<ActionResult<List<Student>>> Get()
+        {
+            return Ok(students);
+        }
+
+
+        [HttpGet("GetStudentbyid/{id}")]//get by id
+        public async Task<IActionResult> GetStudentbyid([FromRoute] int id)
+        {
+            Student? std = students.Find(i => i.id == id);
+            return Ok(std);
+        }
+
+
+        //[HttpGet("GetbyRollno/{rollno}")]//get by roll no
+        //public async Task<ActionResult<Student>> GetbyRollno([FromQuery] int rollno)
+        //{
+        //    Student? st = students.Find(a => a.Rollno == rollno);
+        //    return Ok(st);
+        //}
+
+
+        //[HttpPost]
+        //public async Task<Student> AddData([FromQuery] Student value)
+        //{
+        //    return students.Add(value);
+        //}
+
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Student>> Deletebyid([FromQuery] int id)
+        {
+            Student? s = students.Find(i => i.id == id);
+
+            return Ok(students.Remove(s));
+        }
+    }
+}
---
+++ FirstApi/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstApi.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string StudentName { get; set; } = string.Empty;
+        public string FatherName { get; set; } = string.Empty;
+
+        public int Rollno { get; set; }
+    }
+}
